d121ba753da20d4913337b015f752441a95006c2
Tech Stuff <teknixstuff+accounts.github@gmail.com>
2022-11-05 22:22:02 +0100

    Add CWSM

---
+++ CWSM.py
@@ -0,0 +1,19 @@
+#PLUGINCONFIG#
+# name=Logger #
+# ver=1.0.0   #
+#/PLUGINCONFIG#
+
+import discord
+from discord.ext import commands
+from libover import plugin_perms
+plugin_perms.guildID = guildID
+import json
+import datetime
+import re
+
+client = commands.Bot(command_prefix=['/logger;'], intents=discord.Intents().all())
+
+@client.command(name='curseforgecwsm')
+@plugin_perms.check_guild
+async def adduser(ctx, user):
+  ctx.reply('https://www.curseforge.com/minecraft/mc-mods/crackers-wither-storm-mod')
