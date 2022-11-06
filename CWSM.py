af5728235635bce09010f4d6dff5c946a97a8adc
Tech Stuff <teknixstuff+accounts.github@gmail.com>
2022-11-06 08:46:33 +0100

    Fix plugin meta

--- CWSM.py
+++ CWSM.py
@@ -1,19 +1,16 @@
 #PLUGINCONFIG#
-# name=Logger #
-# ver=1.0.0   #
+# name=CWSM  #
+# ver=1.0.0  #
 #/PLUGINCONFIG#
 
 import discord
 from discord.ext import commands
 from libover import plugin_perms
 plugin_perms.guildID = guildID
-import json
-import datetime
-import re
 
 client = commands.Bot(command_prefix=['/CWSM:','.?'], intents=discord.Intents().all())
 
 @client.command(name='curseforgecwsm')
 @plugin_perms.check_guild
-async def adduser(ctx, user):
+async def curseforgecwsm(ctx, user):
   ctx.reply('https://www.curseforge.com/minecraft/mc-mods/crackers-wither-storm-mod')
